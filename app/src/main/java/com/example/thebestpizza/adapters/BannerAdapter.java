93f57d420aeb09d898367a65d13cd25eb7f58060
Avt0r <ruslanmotriuk@gmail.com>
2022-10-13 20:45:29 +0300

    .

--- app/src/main/java/com/example/thebestpizza/adapters/BannerAdapter.java
+++ app/src/main/java/com/example/thebestpizza/adapters/BannerAdapter.java
@@ -1,6 +1,11 @@
 package com.example.thebestpizza.adapters;
 
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
 public class BannerAdapter extends ArrayAdapter {
+
+    public BannerAdapter(Context context, int res){
+        super(context,res);
+    }
 }
--- app/src/main/java/com/example/thebestpizza/adapters/ItemAdapter.java
+++ app/src/main/java/com/example/thebestpizza/adapters/ItemAdapter.java
@@ -1,6 +1,11 @@
 package com.example.thebestpizza.adapters;
 
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
 public class ItemAdapter extends ArrayAdapter {
+
+    public ItemAdapter(Context context, int res){
+        super(context,res);
+    }
 }
