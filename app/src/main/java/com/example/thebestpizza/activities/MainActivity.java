6f1037124764d02702bcc369d49d3383141ffaf4
Avt0r <ruslanmotriuk@gmail.com>
2022-10-14 22:28:17 +0300

    .

--- app/src/main/java/com/example/thebestpizza/activities/MainActivity.java
+++ app/src/main/java/com/example/thebestpizza/activities/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.thebestpizza.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.example.thebestpizza.R;
+import com.example.thebestpizza.adapters.BannerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView bannerList;
+    List<Drawable> images = new ArrayList<>();
+
+    @SuppressLint({"ResourceType", "UseCompatLoadingForDrawables"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        images.add(getDrawable(R.drawable.img));
+        images.add(getDrawable(R.drawable.img_1));
+        images.add(getDrawable(R.drawable.img_2));
+        images.add(getDrawable(R.drawable.img_3));
+        bannerList = findViewById(R.id.bannerList);
+        BannerAdapter adapter = new BannerAdapter(this, images);
+        bannerList.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/thebestpizza/adapters/BannerAdapter.java
+++ app/src/main/java/com/example/thebestpizza/adapters/BannerAdapter.java
@@ -1,11 +1,56 @@
 package com.example.thebestpizza.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BannerAdapter extends ArrayAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
-    public BannerAdapter(Context context, int res){
-        super(context,res);
+import com.example.thebestpizza.R;
+
+import java.util.List;
+
+
+public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.ViewHolder> {
+
+    private final LayoutInflater inflater;
+    private final List<Drawable> images;
+
+    public BannerAdapter(Context context, List<Drawable> images){
+        this.images = images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public BannerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = inflater.inflate(R.layout.banner_list_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull BannerAdapter.ViewHolder holder, int position) {
+        holder.image.setImageDrawable(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        ViewHolder(View view){
+            super(view);
+            image = view.findViewById(R.id.banner_list_item_image);
+        }
     }
 }
